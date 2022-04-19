7e8df70c5d528ce3a900a50254aea53852df72b7
Daniel <daniel-re@outlook.com>
2022-04-18 22:06:01 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          What's Up With Mars?
+        </p>
+        <p>
+          Find out soon!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
